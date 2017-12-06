1bcd4db305d964e8d4aa9a3718c6fe3da25d353a
Niels <niels95debruyne@hotmail.com>
2017-12-07 00:22:50 +0100

    make it better

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ require('./backend/config/passport');
 var index = require('./backend/routes/index');
 var users = require('./backend/routes/users');
 
-// view engine setup
-app.set('views', path.join(__dirname / `/backend`, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
